f1a7b15512afa6cf2bfbfbc2f69c83cf28c0eafd
rafigarcia <rafaela.garcia.angulo@gmail.com>
2024-02-14 22:40:54 -0600

    Week5 Chamber

--- chamber/scripts/menu.js
+++ chamber/scripts/menu.js
@@ -7,3 +7,15 @@ hamButton.addEventListener('click', () => {
 	navigation.classList.toggle('open');
 	hamButton.classList.toggle('open');
 });
+
+
+// DARK MODE 
+
+const modeButton = document.querySelector("#mode");
+const body = document.querySelector("body");
+const main = document.querySelector("main");
+
+modeButton.addEventListener("click", () => {
+	body.classList.toggle('dark-mode');
+	main.classList.toggle('dark-mode');
+});
