fbe119ee89e98dc5c4b38a9ec6a9dc6897f007b2
rafigarcia <rafaela.garcia.angulo@gmail.com>
2024-03-22 17:52:46 -0500

    W10: Chamber Project - Home Page Enhancements

--- chamber/scripts/getdates.js
+++ chamber/scripts/getdates.js
@@ -1,3 +1,6 @@
+//get the date and display last modified
+
+
 const options = {
     weekday: "long",
     day: "numeric",
@@ -14,3 +17,29 @@ const lastModifiedTimestamp = document.lastModified;
 const lastmodifiedDate = new Date(lastModifiedTimestamp);
 
 document.getElementById("lastModified").textContent = lastmodifiedDate.toLocaleDateString("en-US", options);
+
+
+
+// set banner to meetings wednesday
+
+const currentDate = new Date().getDay();
+const banner = document.getElementById("banner");
+
+
+var displayBanner = function() {
+    if (currentDate < 4) {
+        banner.style.display = "block";
+    } else {
+        banner.style.display = "none";
+    }
+}
+
+displayBanner();
+
+
+// Get the close button in the banner
+const closeButton = document.getElementById("closeBtn");
+
+closeButton.addEventListener('click', function() {
+    banner.style.display = "none";
+});
